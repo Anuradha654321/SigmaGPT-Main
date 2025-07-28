d85a1b9d6dfd26677251749609759d4512129245
Vanka Anuradha <anuradhavanka012@gmail.com>
2025-07-29 00:04:22 +0530

    logo push

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -69,7 +69,7 @@ function Sidebar() {
     return (
         <section className="sidebar">
             <button onClick={createNewChat}>
-                <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo"></img>
+                <img src="/blacklogo.png" alt="gpt logo" className="logo"></img>
                 <span><i className="fa-solid fa-pen-to-square"></i></span>
             </button>
 
@@ -94,7 +94,7 @@ function Sidebar() {
             </ul>
  
             <div className="sign">
-                <p>By ApnaCollege &hearts;</p>
+                <p>Sigma GPT &hearts;</p>
             </div>
         </section>
     )
